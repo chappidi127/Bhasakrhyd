830991180db83cb7f9c397d753ccebc17056e1f2
chappidi127 <103403314+chappidi127@users.noreply.github.com>
2022-10-30 10:05:30 +0530

    Update NAGESH.java

--- src/main/java/Practice/NAGESH.java
+++ src/main/java/Practice/NAGESH.java
@@ -6,6 +6,8 @@ public class NAGESH {
 		// TODO Auto-generated method stub
   System.out.println("Test123");
   System.out.println("Test123");
+		 System.out.println("Test123");
+		 System.out.println("Test123");
 	}
 
 }
